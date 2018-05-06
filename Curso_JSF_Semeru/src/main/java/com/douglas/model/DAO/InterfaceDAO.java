0340b12672783e72cc5232e5f29e9a7cd67656bf
DouglasEleuterio <douglas.versato@gmail.com>
2018-05-06 13:37:05 -0300

    Criação da InterfaceDAO e sua Implementação. O processo foi criado de forma generica, de forma que possamor reaproveitar codigo.

---
+++ Curso_JSF_Semeru/src/main/java/com/douglas/model/DAO/InterfaceDAO.java
@@ -0,0 +1,26 @@
+/**
+ * Interface Generica com as operações do CRUD.
+ */
+package com.douglas.model.DAO;
+
+import java.io.Serializable;
+import java.util.List;
+import org.hibernate.criterion.DetachedCriteria;
+
+/**
+ *
+ * @author douglas
+ */
+public interface InterfaceDAO <T> {
+    
+    void save (T entity);
+    void update (T entity);
+    void remove (T entity);
+    void merge (T entity);
+    
+    T getEntity(Serializable id);
+    T getEntityByDetachedCriteria(DetachedCriteria criteria);
+    List<T> getEntities();
+    List<T> getListByDetachedCriteria(DetachedCriteria criteria);
+    
+}
